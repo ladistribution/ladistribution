576c4e3c748143818d62127a5aea3b9362135896
Francois Hodierne <francois@hodierne.net>
2009-03-26 09:08:52 +0000

    packages: add dokuwiki openid plugin
    git-svn-id: http://ladistribution.net/svn/trunk@12 ae0f9bec-b264-4796-a651-e028ba2a50ff

---
+++ packages/dokuwiki/plugin/openid/build.sh
@@ -0,0 +1,16 @@
+SOURCE="git://github.com/znarf/dokuwiki-openid.git"
+FOLDER="plugin"
+PACKAGE="dokuwiki-plugin-openid.zip"
+# Get source
+git clone $SOURCE $FOLDER
+rm -rf $FOLDER/.git
+rm -rf $FOLDER/Makefile
+# Remove Auth libraries as they are handled as dependencies
+rm -Rf $FOLDER/Auth
+# Remove some unwanted files (mac)
+find . -name '*.DS_Store' -type f -delete
+# Create zip package
+zip -rqv $PACKAGE $FOLDER dist
+mv $PACKAGE ../../../
+# Clean
+rm -rf $FOLDER
