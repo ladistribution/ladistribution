665022ed1e44a1358a92e60bd677a3c2cb379a15
Francois Hodierne <francois@hodierne.net>
2009-04-01 21:02:40 +0000

    app: add more actions to Packages controller
    git-svn-id: http://ladistribution.net/svn/trunk@22 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- admin/application/controllers/PackagesController.php
+++ admin/application/controllers/PackagesController.php
@@ -7,11 +7,28 @@ require_once 'BaseController.php';
  */
 class PackagesController extends BaseController
 {
+    public function indexAction()
+    {
+        $this->view->packages = $this->site->getPackages();
+    }
+    
+    public function extensionsAction()
+    {
+        $type = $this->_getParam('type');
+        $packageId = $this->_getParam('id');
+        
+        $this->view->packages = $this->site->getPackageExtensions($packageId, $type);
+    }
     
     public function preferencesAction()
     {
         $type = $this->_getParam('type', 'configuration');
         $packageId = $this->_getParam('id');
+        
+        if ($type == 'install') {
+            $this->view->preferences = $this->site->getInstallPreferences($packageId);
+            return;
+        }
         
         $installer = Ld_Installer_Factory::getInstaller(array('id' => $packageId));
         
@@ -21,5 +38,5 @@ class PackagesController extends BaseController
             $this->view->preferences[] = $pref->toArray(); 
         }
     }
-    
+
 }
