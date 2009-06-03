40f88258e4f97379c19bda6982de5aa495db7bff
Francois Hodierne <francois@hodierne.net>
2009-06-03 13:42:26 +0000

    remove obsolete admin
    git-svn-id: http://ladistribution.net/svn/trunk@56 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- admin/application/views/scripts/error/error.json.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$error = array(
-    'status'  => $this->status,
-    'message' => $this->message,
-    'details' => $this->details
-);
-
-$this->jsonRenderer($error);
--- admin/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once(dirname(__FILE__) . '/dist/prepend.php');
-
-require_once 'application/Bootstrap.php';
-Bootstrap::run();
