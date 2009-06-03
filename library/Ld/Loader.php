5eb4c02361d4eef4f933996c5394d8dbf0b4029c
Francois Hodierne <francois@hodierne.net>
2009-06-03 12:25:34 +0000

    make automatic session desactivable
    git-svn-id: http://ladistribution.net/svn/trunk@49 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- library/Ld/Loader.php
+++ library/Ld/Loader.php
@@ -11,13 +11,15 @@ class Ld_Loader
 
         defined('LD_DEBUG') OR define('LD_DEBUG', true);
 
-        defined('LD_SERVER') OR define('LD_SERVER', 'http://ladistribution.h6e.net/');
-
         defined('LD_REWRITE') OR define('LD_REWRITE', true);
+
+        defined('LD_SESSION') OR define('LD_SESSION', true);
 
         defined('LD_TMP_DIR') OR define('LD_TMP_DIR', realpath( $dir ) . '/tmp');
 
         defined('LD_LIB_DIR') or define('LD_LIB_DIR', realpath( $dir ) . '/lib' );
+
+        defined('LD_SERVER') OR define('LD_SERVER', 'http://ladistribution.h6e.net/');
 
         set_include_path( get_include_path() . PATH_SEPARATOR . LD_LIB_DIR );
     }
@@ -34,8 +36,6 @@ class Ld_Loader
         // TODO: to be replaced by an Autoloader
         require_once 'clearbricks/common/lib.files.php';
         require_once 'clearbricks/zip/class.unzip.php';
-
-        Zend_Session::start();
 
         // Site configuration
         $configFilename = $dir . '/dist/config.json';
@@ -55,22 +55,26 @@ class Ld_Loader
         $site = new Ld_Site_Local($config);
         Zend_Registry::set('site', $site);
 
-        if (function_exists('mcrypt_ecb')) {
-            $cookieManager = new Ld_Cookie('SECRET_KEY', array('cookiePath' => $site->getPath()));
-            $authStorage = new Ld_Auth_Storage_Cookie($cookieManager);
-        } else {
-            $path = $site->getPath();
-            $namespace = empty($path) ? 'default' : $path;
-            $authStorage = new Zend_Auth_Storage_Session($namespace);
+        if (constant('LD_SESSION')) {
+
+            Zend_Session::start();
+
+            if (function_exists('mcrypt_ecb')) {
+                $cookieManager = new Ld_Cookie('SECRET_KEY', array('cookiePath' => $site->getPath()));
+                $authStorage = new Ld_Auth_Storage_Cookie($cookieManager);
+            } else {
+                $path = $site->getPath();
+                $namespace = empty($path) ? 'default' : $path;
+                $authStorage = new Zend_Auth_Storage_Session($namespace);
+            }
+
+            $auth = Zend_Auth::getInstance();
+            $auth->setStorage($authStorage);
+
         }
-        Zend_Registry::set('authStorage', $authStorage);
 
-        $auth = Zend_Auth::getInstance();
-        $auth->setStorage($authStorage);
-
-        // Legacy CSS Constants
-        defined('LD_CSS_URL') OR define('LD_CSS_URL', $site->getUrl('css'));
-        defined('H6E_CSS') OR define('H6E_CSS', LD_CSS_URL);
+        // Legacy CSS Constant
+        defined('H6E_CSS') OR define('H6E_CSS', $site->getUrl('css'));
 
         return $site;
     }
