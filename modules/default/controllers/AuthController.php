4f4a6ba73575397d9f47b1f62ee4b73a51bd2755
Francois Hodierne <francois@hodierne.net>
2009-06-10 15:48:12 +0000

    fix a bug preventing to login after an invalid attempt
    git-svn-id: http://ladistribution.net/svn/trunk@85 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- modules/default/controllers/AuthController.php
+++ modules/default/controllers/AuthController.php
@@ -10,7 +10,14 @@ class AuthController extends Ld_Controller_Action
 
     function loginAction()
     {
-        $this->view->postParams = $_POST;
+        $this->view->postParams = array();
+        foreach ($_POST as $key => $value) {
+            $ignore = array('ld_auth_username', 'ld_auth_password', 'ld_auth_action');
+            if (in_array($key, $ignore)) {
+                continue;
+            }
+            $this->view->postParams[$key] = $value;
+        }
 
         if ($this->getRequest()->isPost() && Zend_Auth::getInstance()->hasIdentity()) {
             $this->_forward('index', 'index', 'default');
