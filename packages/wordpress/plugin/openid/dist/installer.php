f0b0573ba9404507e1a7dd983c7eb4d9ced2fe07
Francois Hodierne <francois@hodierne.net>
2009-06-16 08:16:14 +0000

    wp openid plugin: fix install issue with WP 2.8
    git-svn-id: http://ladistribution.net/svn/trunk@127 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- packages/wordpress/plugin/openid/dist/installer.php
+++ packages/wordpress/plugin/openid/dist/installer.php
@@ -9,7 +9,12 @@ class Installer_Plugin_Openid extends Ld_Installer
 	{
 		if (empty($this->loaded)) {
 
+			define('WP_LD_INSTALLER', true);
+
 			global $wpdb, $wp_version, $wp_rewrite, $wp_db_version, $wp_taxonomies, $wp_filesystem, $wp_roles;
+
+			// fix 'Wrong datatype for second argument in wp-includes/widgets.php on lines 607'
+			global $_wp_deprecated_widgets_callbacks;
 
 			require_once $this->absolutePath . "/../../../wp-load.php";
 			require_once $this->absolutePath . "/../../../wp-admin/includes/plugin.php";
