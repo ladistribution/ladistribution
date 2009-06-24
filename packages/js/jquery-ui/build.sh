4ec49e5d771f9f9acba5f0b8c4e630686169f11f
Francois Hodierne <francois@hodierne.net>
2009-06-24 11:18:12 +0000

    new jquery-ui package (and add dependance in admin)
    git-svn-id: http://ladistribution.net/svn/trunk@149 ae0f9bec-b264-4796-a651-e028ba2a50ff

---
+++ packages/js/jquery-ui/build.sh
@@ -0,0 +1,29 @@
+VERSION="1.7.2"
+FOLDER="js"
+NAME="jquery-ui"
+ZIP="$NAME-$VERSION.custom.zip"
+SOURCE="http://jqueryui.com/download/$ZIP"
+PACKAGE="js-$NAME.zip"
+BUILD="build"
+
+# Get source
+curl $SOURCE > $ZIP
+unzip $ZIP -d $BUILD
+rm $ZIP
+
+# Copy the files we really want
+mkdir $FOLDER
+cp "$BUILD/js/$NAME-$VERSION.custom.min.js" "$FOLDER/jquery-ui.js"
+rm -rf $BUILD
+
+# We don't handle CSS for now (maybe in a separate package)
+
+# Remove some unwanted files (mac)
+find . -name '*.DS_Store' -type f -delete
+
+# Create zip package
+zip -rqv $PACKAGE $FOLDER manifest.xml
+mv $PACKAGE ../../
+
+# Clean
+rm -rf $FOLDER
