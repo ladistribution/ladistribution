e40f727b08b1e7fc3f0a8e45bf7d68358f4e7ec7
Francois Hodierne <francois@hodierne.net>
2009-06-30 11:20:46 +0000

    wordpress: ability to register from wordpress
    git-svn-id: http://ladistribution.net/svn/trunk@159 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- packages/wordpress/application/plugins/ld-auth.php
+++ packages/wordpress/application/plugins/ld-auth.php
@@ -3,7 +3,7 @@
 Plugin Name: LD auth
 Plugin URI: http://h6e.net/wordpress/plugins/ld-auth
 Description: Handle authentification through La Distribution backend
-Version: 0.2-25-3
+Version: 0.2-27-1
 Author: h6e
 Author URI: http://h6e.net/
 */
@@ -108,25 +108,20 @@ function get_userdata( $user_id )
 
 	if ( !$wp_user = $wpdb->get_row($wpdb->prepare("SELECT * FROM $wpdb->users WHERE ID = %d LIMIT 1", $user_id)) )
 		return false;
-
-	foreach (Zend_Registry::get('site')->getUsers() as $ld_user) {
-
-		if ($ld_user['username'] == $wp_user->user_login) {
-
-			$wp_user->user_pass  		= $ld_user['hash'];
-			$wp_user->user_nicename		= $ld_user['username'];
-			$wp_user->display_name		= $ld_user['fullname'];
-			$wp_user->nickname			= $ld_user['username'];
-			$wp_user->user_email		= $ld_user['email'];
-
-			_fill_user($wp_user);
-
-			return $wp_user;
-		}
-
+	
+	$ld_user = Zend_Registry::get('site')->getUser($wp_user->user_login);
+	if ($ld_user) {
+		$wp_user->user_pass  		= $ld_user['hash'];
+		$wp_user->user_nicename		= $ld_user['username'];
+		$wp_user->display_name		= $ld_user['fullname'];
+		$wp_user->nickname			= $ld_user['username'];
+		$wp_user->user_email		= $ld_user['email'];
 	}
 
-	wp_die( __('User not found in LD user backend.') );
+	_fill_user($wp_user);
+	return $wp_user;
+
+	// wp_die( __('get_userdata: User not found in LD user backend.') );
 }
 endif;
 
@@ -139,30 +134,30 @@ function get_userdatabylogin( $user_login )
 
 	if ( empty( $user_login ) )
 		return false;
+	
+	$ld_user = Zend_Registry::get('site')->getUser($user_login);
 
-	foreach (Zend_Registry::get('site')->getUsers() as $ld_user) {
+	if ($ld_user) {
 
-		if ($ld_user['username'] == $user_login) {
+		// if user doesn't exists in DB, we insert it
+		$query = $wpdb->prepare("SELECT * FROM $wpdb->users WHERE user_login = %s LIMIT 1", $ld_user['username']);
 
-			// if user doesn't exists in DB, we insert it
-			$query = $wpdb->prepare("SELECT * FROM $wpdb->users WHERE user_login = %s LIMIT 1", $ld_user['username']);
+		if ( !$wp_user = $wpdb->get_row($query) ) {
+			$data = array('user_login' => $ld_user['username']);
+			$wpdb->insert( $wpdb->users, $data);
+			$user_id = (int) $wpdb->insert_id;
 
-			if ( !$wp_user = $wpdb->get_row($query) ) {
-				$data = array('user_login' => $ld_user['username']);
-				$wpdb->insert( $wpdb->users, $data);
-				$user_id = (int) $wpdb->insert_id;
+			$wp_user = new WP_User($user_id);
+			$wp_user->set_role(get_option('default_role'));
 
-				$wp_user = new WP_User($user_id);
-				$wp_user->set_role('subscriber');
-
-				return get_userdata($user_id);
-			}
-
-			return get_userdata($wp_user->ID);
+			return get_userdata($user_id);
 		}
+
+		return get_userdata($wp_user->ID);
 	}
-    
-	wp_die( __('User not found in LD user backend.') );
+
+	return false;
+	// wp_die( __('get_userdatabylogin: User not found in LD user backend.') );
 }
 endif;
 
@@ -180,3 +175,46 @@ function ld_get_user_by_openid( $openid , $id = null )
 }
 
 add_filter('openid_get_user_by_openid', 'ld_get_user_by_openid');
+
+function ld_get_wp_user($user_id)
+{
+	global $wpdb;
+
+	if ( !$wp_user = $wpdb->get_row($wpdb->prepare("SELECT * FROM $wpdb->users WHERE ID = %d LIMIT 1", $user_id)) )
+		return false;
+
+	$user = array(
+		'hash' 		=> $wp_user->user_pass,
+		'username' 	=> $wp_user->user_nicename,
+		'fullname' 	=> $wp_user->display_name,
+		'email'		=> $wp_user->user_email
+	);
+	
+	return $user;
+}
+
+function ld_user_register($user_id)
+{
+	$user = ld_get_wp_user($user_id);
+	Zend_Registry::get('site')->addUser($user);
+}
+
+add_action('user_register', 'ld_user_register');
+
+function ld_profile_update($user_id)
+{
+	$user = ld_get_wp_user($user_id);
+	Zend_Registry::get('site')->updateUser($user['username'], $user);
+}
+
+add_action('profile_update', 'ld_profile_update');
+
+if ( !function_exists('wp_set_password') ) :
+function wp_set_password( $password, $user_id ) {
+	global $wpdb;
+	$hash = wp_hash_password($password);
+	$wpdb->update($wpdb->users, array('user_pass' => $hash, 'user_activation_key' => ''), array('ID' => $user_id) );
+	wp_cache_delete($user_id, 'users');
+	ld_profile_update($user_id);
+}
+endif;
