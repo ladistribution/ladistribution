55b7faef42932c844d7c3d26cc17e4802677a49a
Francois Hodierne <francois@hodierne.net>
2009-07-09 12:58:35 +0000

    wordpress: add a geshi based codecolorer plugin
    git-svn-id: http://ladistribution.net/svn/trunk@181 ae0f9bec-b264-4796-a651-e028ba2a50ff

---
+++ packages/wordpress/plugin/codecolorer/build.sh
@@ -0,0 +1,19 @@
+NAME="codecolorer"
+VERSION="0.7.3"
+SOURCE="http://svn.wp-plugins.org/$NAME/tags/$VERSION/"
+FOLDER="plugin"
+PACKAGE="wordpress-plugin-$NAME.zip"
+# Get source
+svn export $SOURCE $FOLDER --force
+# Remove Geshi
+rm -rf $FOLDER/lib
+# Apply Geshi patch
+patch -p0 -d $FOLDER < patches/geshi.diff
+# Remove locales
+rm -rf $FOLDER/codecolorer-ru_RU.mo
+rm -rf $FOLDER/codecolorer-ru_RU.po
+# Create zip package
+zip -rqv $PACKAGE $FOLDER dist -x "*/.svn/*"
+mv $PACKAGE ../../../
+# Clean
+rm -rf $FOLDER
