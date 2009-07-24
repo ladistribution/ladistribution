790bd61cbc0b52e6f88a411603e2373eb91aac7a
Francois Hodierne <francois@hodierne.net>
2009-07-24 15:05:47 +0000

    add scripts to import packages in repositories
    git-svn-id: http://ladistribution.net/svn/trunk@228 ae0f9bec-b264-4796-a651-e028ba2a50ff

---
+++ packages/import.contrib.sh
@@ -0,0 +1,19 @@
+PATH=$PATH:../bin
+
+SITE=".."
+REPOSITORY="contrib"
+
+IMPORT="ladis import-package --site $SITE $REPOSITORY"
+
+$IMPORT bbpress.zip
+$IMPORT bbpress-theme-minimal.zip
+
+$IMPORT habari.zip
+$IMPORT dotclear.zip
+$IMPORT laconica.zip
+$IMPORT spip.zip
+$IMPORT indexhibit.zip
+$IMPORT moonmoon.zip
+$IMPORT gallery.zip
+
+$IMPORT wordpress-plugin-codecolorer.zip
---
+++ packages/import.main.sh
@@ -0,0 +1,39 @@
+PATH=$PATH:../bin
+
+SITE=".."
+REPOSITORY="main"
+
+IMPORT="ladis import-package --site $SITE $REPOSITORY"
+
+$IMPORT admin.zip
+
+$IMPORT lib-zend-framework.zip
+$IMPORT lib-clearbricks.zip
+$IMPORT lib-ld.zip
+$IMPORT lib-php-openid.zip
+$IMPORT lib-geshi.zip
+
+$IMPORT css-h6e-minimal.zip
+$IMPORT css-ld-ui.zip
+
+$IMPORT js-jquery.zip
+$IMPORT js-jquery-ui.zip
+$IMPORT js-jquery-tablednd.zip
+
+$IMPORT wordpress.zip
+$IMPORT wordpress-plugin-openid.zip
+$IMPORT wordpress-theme-minimal.zip
+$IMPORT wordpress-theme-p2.zip
+$IMPORT wordpress-theme-hemingway.zip
+$IMPORT wordpress-theme-journalist.zip
+$IMPORT wordpress-theme-simpla.zip
+$IMPORT wordpress-theme-whiteasmilk.zip
+
+$IMPORT dokuwiki.zip
+$IMPORT dokuwiki-plugin-openid.zip
+$IMPORT dokuwiki-plugin-css.zip
+$IMPORT dokuwiki-theme-minimal.zip
+$IMPORT dokuwiki-theme-monobook.zip
+
+$IMPORT bbpress.zip
+$IMPORT bbpress-theme-minimal.zip
