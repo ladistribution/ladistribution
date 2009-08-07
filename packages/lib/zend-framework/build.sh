023444ee84b6bafd2377285e813d116bb896d91c
Francois Hodierne <francois@hodierne.net>
2009-08-07 07:46:48 +0000

    zend framework 1.9
    git-svn-id: http://ladistribution.net/svn/trunk@250 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- packages/lib/zend-framework/build.sh
+++ packages/lib/zend-framework/build.sh
@@ -1,16 +1,19 @@
 FOLDER="lib"
 PACKAGE="lib-zend-framework.zip"
-VERSION="ZendFramework-1.8.2"
+VERSION="ZendFramework-1.9.0"
 NAME="$VERSION-minimal"
 ZIP="$NAME.zip"
+
 # Get source
 curl http://framework.zend.com/releases/$VERSION/$ZIP > $ZIP
 unzip $ZIP
 rm $ZIP
 mv $NAME/library $FOLDER
 rm -rf $NAME
+
 # Create zip package
 zip -rqv $PACKAGE $FOLDER manifest.xml
 mv $PACKAGE ./../../
+
 # Clean
 rm -rf $FOLDER
