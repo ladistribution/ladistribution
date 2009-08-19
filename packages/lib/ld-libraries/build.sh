bc3f176c60c22921c3bd4c3bbb15c40ce7468a68
Francois Hodierne <francois@hodierne.net>
2009-08-19 08:09:44 +0000

    update core packages
    git-svn-id: http://ladistribution.net/svn/trunk@274 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- packages/lib/ld-libraries/build.sh
+++ packages/lib/ld-libraries/build.sh
@@ -1,12 +1,19 @@
 SOURCE="http://ladistribution.net/svn/trunk/lib/"
 FOLDER="lib"
 PACKAGE="lib-ld.zip"
+
 # Export from SVN
 svn export $SOURCE $FOLDER
+
+# Get locales
+svn export "http://ladistribution.net/svn/trunk/shared/locales/ld" locales
+
 # Remove some unwanted files (mac)
 find . -name '*.DS_Store' -type f -delete
+
 # Create zip package
-zip -rqv $PACKAGE $FOLDER manifest.xml -x "*/.svn/*"
+zip -rqv $PACKAGE $FOLDER locales manifest.xml -x "*/.svn/*"
 mv $PACKAGE ../../
+
 # Clean
 rm -rf $FOLDER
