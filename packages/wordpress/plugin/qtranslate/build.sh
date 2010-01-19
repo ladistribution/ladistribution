daee707c81b3766a519492e56b072683e9f861f4
Francois Hodierne <francois@hodierne.net>
2010-01-19 16:27:23 +0000

    qtranslate 2.5.5
    git-svn-id: http://ladistribution.net/svn/trunk@378 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- packages/wordpress/plugin/qtranslate/build.sh
+++ packages/wordpress/plugin/qtranslate/build.sh
@@ -1,12 +1,12 @@
 NAME="qtranslate"
-VERSION="2.5.4"
+VERSION="2.5.5"
 SOURCE="http://svn.wp-plugins.org/$NAME/tags/$VERSION/"
 FOLDER="plugin"
 PACKAGE="wordpress-plugin-$NAME.zip"
 # Get source
-# svn export $SOURCE $FOLDER --force
+svn export $SOURCE $FOLDER --force
 # Alternate source
-cp -R /Web/wordpress-trunk/wp-content/plugins/qtranslate $FOLDER
+# cp -R /Web/wordpress-trunk/wp-content/plugins/qtranslate $FOLDER
 # Create zip package
 zip -rqv $PACKAGE $FOLDER dist -x "*/.svn/*"
 mv $PACKAGE ../../../
