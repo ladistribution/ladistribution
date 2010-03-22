394c3f91d47a5944dd5c583c8421e224ac0a8517
Francois Hodierne <francois@hodierne.net>
2010-03-22 00:52:56 +0000

    clean index.php
    git-svn-id: http://ladistribution.net/svn/trunk@432 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-ini_set("memory_limit", "128M");
 define('LD_ROOT_CONTEXT', true);
 if (file_exists('admin/dispatch.php')) require_once('admin/dispatch.php');
 else echo 'La Distribution Admin component not installed.';
-// $cfgFile = $site->getDirectory('dist') . "/config.json";
-// $config = Ld_Files::getJson($cfgFile);
-// $config['root_admin'] = false;
-// $config['root_application'] = '';
-// Ld_Files::putJson($cfgFile, $config);
