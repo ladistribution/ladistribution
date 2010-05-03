e470181cb1336b6f3e03ec263bffd9eba06bfb31
Francois Hodierne <francois@hodierne.net>
2010-05-03 11:53:12 +0000

    fix various localisation issues
    git-svn-id: http://ladistribution.net/svn/trunk@500 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- packages/locales/fr-fr/wordpress/build.sh
+++ packages/locales/fr-fr/wordpress/build.sh
@@ -1,7 +1,7 @@
 NAME="wordpress"
 LOCALE="fr_FR"
-VERSION="2.9.1"
-SOURCE="http://svn.automattic.com/wordpress-i18n/$LOCALE/tags/$VERSION/"
+VERSION="3.0"
+SOURCE="http://svn.automattic.com/wordpress-i18n/$LOCALE/branches/$VERSION/"
 PACKAGE="$NAME-locale-fr-fr.zip"
 
 # Get source
@@ -10,6 +10,8 @@ svn export "$SOURCE/messages/$LOCALE.mo" "languages/$LOCALE.mo"
 svn export "$SOURCE/messages/continents-cities-$LOCALE.mo" "languages/continents-cities-$LOCALE.mo"
 mkdir themes/default
 svn export "$SOURCE/messages/kubrick/$LOCALE.mo" "themes/default/$LOCALE.mo"
+mkdir themes/twentyten
+svn export "$SOURCE/messages/twentyten/$LOCALE.mo" "themes/twentyten/$LOCALE.mo"
 
 # Remove some unwanted files (mac)
 find . -name '*.DS_Store' -type f -delete
