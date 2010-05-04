87c1798dd4c90dd6ff1075e134be899bffb6486a
Francois Hodierne <francois@hodierne.net>
2010-05-04 12:29:41 +0000

    update h6e-minimal. add htaccess based header optimisation
    git-svn-id: http://ladistribution.net/svn/trunk@503 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- packages/css/h6e-minimal/build.sh
+++ packages/css/h6e-minimal/build.sh
@@ -7,7 +7,7 @@ rm -rf $FOLDER/.git
 # Remove some unwanted files (mac)
 find . -name '*.DS_Store' -type f -delete
 # Create zip package
-zip -rqv $PACKAGE $FOLDER manifest.xml
+zip -rqv $PACKAGE $FOLDER manifest.xml -x \*.svn/\* \*.preserve
 mv $PACKAGE ../../
 #Clean
 rm -rf $FOLDER
