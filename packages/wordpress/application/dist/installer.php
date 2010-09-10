4a1450dfa97e4ddd48cebb114d998de62c41e017
Francois Hodierne <francois@hodierne.net>
2010-09-10 12:25:20 +0000

    wpml/sitepress support
    git-svn-id: http://ladistribution.net/svn/trunk@596 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- packages/wordpress/application/dist/installer.php
+++ packages/wordpress/application/dist/installer.php
@@ -422,7 +422,7 @@ class Ld_Installer_Wordpress_Plugin extends Ld_Installer
 			global $wpdb, $wp_embed;
 			global $blog_id, $table_prefix;
 			global $_wp_deprecated_widgets_callbacks;
-			global $q_config;
+			global $q_config, $sitepress;
 			global $PHP_SELF;
 			require_once $this->getAbsolutePath() . "/../../../wp-load.php";
 			require_once $this->getAbsolutePath() . "/../../../wp-admin/includes/plugin.php";
