217c414136ce931af78a086ccf36e5a878d4f551
Francois Hodierne <francois@hodierne.net>
2010-09-17 08:25:33 +0000

    conditional instance navigation
    git-svn-id: http://ladistribution.net/svn/trunk@607 ae0f9bec-b264-4796-a651-e028ba2a50ff

--- shared/modules/slotter/views/helpers/InstanceNavigation.php
+++ shared/modules/slotter/views/helpers/InstanceNavigation.php
@@ -5,18 +5,31 @@ class View_Helper_InstanceNavigation extends Zend_View_Helper_Abstract
 
     public function instanceNavigation()
     {
+        $site = $this->view->site;
         $instance = $this->view->instance;
+
+        $configuration = $instance->getConfiguration();
+        $themes = $instance->getThemes();
+        $extensions = $site->getPackageExtensions( $instance->getPackageId() );
+        $roles = $instance->getRoles();
 
         echo '<ul class="ld-instance-menu">' . "\n";
 
-        $actions = array(
-            'status'     => $this->translate('status'),
-            'configure'  => $this->translate('configure'),
-            'themes'     => $this->translate('themes'),
-            'extensions' => $this->translate('extensions'),
-            'roles'      => $this->translate('roles'),
-            'backups'    => $this->translate('backups')
-        );
+        $actions = array();
+        $actions['status'] = $this->translate('status');
+        if (!empty($configuration)) {
+            $actions['configure'] = $this->translate('configure');
+        }
+        if (!empty($themes)) {
+            $actions['themes'] = $this->translate('themes');
+        }
+        if (!empty($extensions)) {
+            $actions['extensions'] = $this->translate('extensions');
+        }
+        if (!empty($roles)) {
+            $actions['roles'] = $this->translate('roles');
+        }
+        $actions['backups']   =  $this->translate('backups');
 
         foreach ($actions as $action => $label) {
             $url = $this->view->instanceActionUrl($action, $this->view->id);
