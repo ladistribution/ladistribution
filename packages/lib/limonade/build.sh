0ff8562a1246b2ffa6aad6a424f2b057a114af1f
Francois Hodierne <francois@hodierne.net>
2010-11-24 10:01:42 +0000

    add limonade library
    git-svn-id: http://ladistribution.net/svn/trunk@736 ae0f9bec-b264-4796-a651-e028ba2a50ff

---
+++ packages/lib/limonade/build.sh
@@ -0,0 +1,30 @@
+VERSION="0.5"
+FOLDER="lib"
+NAME="limonade"
+SOURCE="git://github.com/sofadesign/limonade.git"
+ZIP="$NAME.zip"
+PACKAGE="lib-$NAME.zip"
+BUILD="build"
+
+echo "# Building $NAME package"
+
+echo "# Get source from $SOURCE with git"
+git clone --quiet git://github.com/sofadesign/limonade.git $BUILD
+cd $BUILD
+git checkout 0.5-stable
+cd ..
+
+# Grab files we want
+mv "$BUILD/lib/limonade" $FOLDER
+mv "$BUILD/lib/limonade.php" limonade.php
+rm -rf "$BUILD"
+
+# Remove some unwanted files (mac)
+find . -name '*.DS_Store' -type f -delete
+
+echo "# Packing $PACKAGE"
+zip -rq $PACKAGE $FOLDER manifest.xml limonade.php
+mv $PACKAGE ../../
+
+# Clean
+rm -rf $FOLDER limonade.php
