e775db278d48a3637f78f16c9edf796135f4bda5
Francois Hodierne <francois@hodierne.net>
2011-01-11 20:46:37 +0000

    bouncer + rediska
    git-svn-id: http://ladistribution.net/svn/trunk@799 ae0f9bec-b264-4796-a651-e028ba2a50ff

---
+++ packages/lib/bouncer/build.sh
@@ -0,0 +1,23 @@
+FOLDER="lib"
+NAME="bouncer"
+SOURCE="git@github.com:znarf/Bouncer.git"
+ZIP="$NAME.zip"
+PACKAGE="lib-bouncer.zip"
+
+echo "# Building $NAME package"
+
+echo "# Get source from $SOURCE with git"
+git clone $SOURCE $FOLDER --quiet
+rm -rf $FOLDER/.git
+
+mv $FOLDER/images images
+
+# Remove some unwanted files (mac)
+find . -name '*.DS_Store' -type f -delete
+
+echo "# Packing $PACKAGE"
+zip -r $PACKAGE $FOLDER manifest.xml
+mv $PACKAGE ../../
+
+# Clean
+rm -rf $FOLDER images
---
+++ packages/lib/rediska/build.sh
@@ -0,0 +1,29 @@
+VERSION="0.5.1"
+FOLDER="lib"
+NAME="rediska"
+SOURCE="http://rediska.geometria-lab.ru/download/Rediska-0-5-1.zip"
+ZIP="$NAME.zip"
+PACKAGE="lib-$NAME.zip"
+BUILD="Rediska $VERSION"
+
+echo "# Building $NAME package"
+
+echo "# Get source from $SOURCE with curl"
+curl $SOURCE --user-agent "La Distribution Build system (http://ladistribution.net/)" -# > $ZIP
+unzip $ZIP
+rm $ZIP
+
+# Grab files we want
+mv "$BUILD/library/Rediska" $FOLDER
+mv "$BUILD/library/Rediska.php" Rediska.php
+rm -rf "$BUILD"
+
+# Remove some unwanted files (mac)
+find . -name '*.DS_Store' -type f -delete
+
+echo "# Packing $PACKAGE"
+zip -r $PACKAGE $FOLDER manifest.xml Rediska.php
+mv $PACKAGE ../../
+
+# Clean
+rm -rf $FOLDER Rediska.php
